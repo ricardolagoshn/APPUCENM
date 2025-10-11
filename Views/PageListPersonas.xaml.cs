0faaf4a3bb88bc5dc98581174961686dac0b6821
Dell <ricardo.lagoshn@gmail.com>
2025-10-10 21:11:19 -0600

    Creacion de objeto de lista

---
+++ Views/PageListPersonas.xaml.cs
@@ -0,0 +1,29 @@
+namespace APPUCENM.Views;
+
+public partial class PageListPersonas : ContentPage
+{
+	public PageListPersonas()
+	{
+		InitializeComponent();
+	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void ToolbarItem_Clicked_2(object sender, EventArgs e)
+    {
+
+    }
+
+    private void listapersonas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+}

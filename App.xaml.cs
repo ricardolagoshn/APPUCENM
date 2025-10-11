5abd31d320616e71e97593e58a3a200c369de306
Dell <ricardo.lagoshn@gmail.com>
2025-10-10 20:58:21 -0600

    Cambios en App.xml

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,6 +2,24 @@
 {
     public partial class App : Application
     {
+
+        static Controllers.PersonasController database;
+
+        public static Controllers.PersonasController Database
+        {
+            get 
+            {
+                if (database == null)
+                {
+                    database = new Controllers.PersonasController();
+                }
+
+                return database;
+            }
+            
+        }
+
+
         public App()
         {
             InitializeComponent();
